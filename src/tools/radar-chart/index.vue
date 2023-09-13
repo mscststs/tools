53e5471dc8c30cd1b781e385b0c8201be0049671
mscststs <github@mscststs.com>
2023-09-13 15:21:32 +0800

    add:proxy

--- src/tools/radar-chart/index.vue
+++ src/tools/radar-chart/index.vue
@@ -32,7 +32,7 @@ function getTimeVariables(date: Date) {
 const form = reactive({
   selectArea: mmcRadar[0].name,
   selectDuration: 3 * 60 * 60 * 1000,
-  selectedProxy: "wsrv"
+  selectedProxy: "appmask"
 });
 
 const selectArea = computed<UrlTemplate | null>(() => {
@@ -271,6 +271,7 @@ async function record() {
         <option :value="15 * 24 * 60 * 60 * 1000">15d</option>
       </select>
       <select class="select select-primary" v-model="form.selectedProxy">
+        <option value="appmask">线路1：appmask（慢）</option>
         <option value="baidu">线路2：百度（不支持录制和下载）</option>
         <option value="wsrv">线路3：wsrv.nl（不稳定）</option>
         <option value="">线路4：原始地址（不支持 HTTPS）</option>
--- src/utils/image.ts
+++ src/utils/image.ts
@@ -41,17 +41,20 @@ const baiduProxy = (url: string) => {
   return noProxy(`https://image.baidu.com/search/down?url=${url}`);
 };
 
+const appmaskProxy = (url: string) => {
+  return noProxy(`https://nmcimage.fbi-warning.eu.org/search/down?url=${url}`, "anonymous");
+};
+
 const wsrvProxy = async (url: string) => {
-  return noProxy(`https://wsrv.nl/?url=${url}`);
+  return noProxy(`https://wsrv.nl/?url=${url}`, "anonymous");
 };
 
 const mscNmcProxy = async (url: string) => {
   const u = new URL(url);
   u.host = "nmc-image.guangzhou-cvm.mscststs.com";
   u.protocol = "https:";
-  return noProxy(u.href);
+  return noProxy(u.href, "anonymous");
 };
-
 
 export const getImage = async (url: string, proxy: string): Promise<HTMLImageElement> => {
   if (proxy === "mscnmc") {
@@ -62,6 +65,9 @@ export const getImage = async (url: string, proxy: string): Promise<HTMLImageEle
   }
   if (proxy === "wsrv") {
     return await wsrvProxy(url);
+  }
+  if (proxy === "appmask") {
+    return await appmaskProxy(url);
   }
   return await noProxy(url, "anonymous");
 };
